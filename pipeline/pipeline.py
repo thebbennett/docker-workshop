b5e7d46d7063a49f6fc6abd8235f4090bafe126c
brittany bennett <thebrittanybennett@gmail.com>
2026-02-05 15:53:38 +0000

    following along with video

---
+++ pipeline/pipeline.py
@@ -0,0 +1,13 @@
+import sys 
+
+import pandas as pd
+
+print('arguments', sys.argv)
+
+month = int(sys.argv[1])
+
+df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+print(df.head())
+
+df.to_parquet(f"output_{month}.parquet")
+print(f'hello pipeline, month={month}')
